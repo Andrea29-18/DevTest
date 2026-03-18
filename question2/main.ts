a1aec047f690789bd8e3a7ea6f6379ae691994ff
Andrea29-18 <aavp0316@gmail.com>
2026-03-18 16:21:13 -0600

    feat:  Question 2 exercise completed

--- question2/main.ts
+++ question2/main.ts
@@ -1,3 +1,9 @@
 export function findOutlier(integers: number[]): number {
-    
-  }
+
+  // Filter the even and odd numbers from the input array
+  const even = integers.filter(x => x % 2 === 0);
+  const odd = integers.filter(x => x % 2 !== 0);
+
+  // Return the single even number if there is only one, otherwise return the single odd number
+  return even.length === 1 ? even[0] : odd[0];
+}
