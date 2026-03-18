cd8f42e7858373729352f711c7527a133fd833f4
Andrea29-18 <aavp0316@gmail.com>
2026-03-18 15:56:26 -0600

    feat: First exercise completed

--- question1/main.ts
+++ question1/main.ts
@@ -1,4 +1,20 @@
 export function isValidWalk(walk: string[]) {
-  
-}
 
+    //First, make sure there are 10 steps
+    if (walk.length !== 10) {
+        return false;
+    }
+
+    //Count the number of steps in each direction and compare them
+    const northSteps = walk.filter(step => step === 'n').length;
+    const southSteps = walk.filter(step => step === 's').length;
+    const eastSteps = walk.filter(step => step === 'e').length;
+    const westSteps = walk.filter(step => step === 'w').length;
+
+    /** 
+        To return to the starting point, the number of north steps must equal the number of south steps
+        And the number of east steps must equal the number of west steps
+    **/
+    return northSteps === southSteps && eastSteps === westSteps;
+
+}
